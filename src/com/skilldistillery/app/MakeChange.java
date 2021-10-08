f73fb5475a35ad29b5cbca97f5b32f54f2f52b20
David Lizon <draggero@gmail.com>
2021-10-08 16:19:58 -0600

    created scanner

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,9 +1,17 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
 
+		Scanner kb = new Scanner(System.in);
+
+		
+		kb.close();
 	}
 
+	
+	
 }
