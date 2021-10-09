a43dcd61356f13d20d92d93b1d90e55a21ba89cb
David Lizon <draggero@gmail.com>
2021-10-09 14:25:01 -0600

    accounted for missing penny when value tendered was # dollars and 1 penny over cost.

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -14,58 +14,40 @@ public class MakeChange {
 		// notify the attendant how many of each piece of currency ($20 ,$10 ,$5 ,$1,
 		// .25c, .10c, .05c, .01c) is needed to make the change for the customer. Change
 		// should be provided using the largest bill and coin denominations as possible.
-		// Denominations that are not used should not be displayed.
-
-		// Hint: Mod operator
+		// Denominations that are not used are not displayed.
 
 		System.out.println("Welcome to DVS, the Digital Void Store!\n");
 
 		double itemCost = 0.0;
 		double moneyGiven = 0.0;
 		String again;
+//		String moneyReturned = "You are receiving back:";
 		boolean repeat = true;
 
-		// REPEATS Q'S AND STUFF
+		// Repeats while user wants to "buy" more stuff.
 		while (repeat == true) {
 
+			String moneyReturned = "You are receiving back:";
+			
 			// User Story #1
 			// The user is prompted asking for the price of the item.
-
 			itemCost = askCost(itemCost);
 
 			// User Story #2
 			// The user is then prompted asking how much money was tendered by the customer.
-
 			moneyGiven = askGiven(moneyGiven);
 
 			// User Story #3
 			// Display an appropriate message if the customer provided too little money or
 			// the exact amount.
-
 			do {
 				if (moneyGiven < itemCost) {
 					System.out.println(
-							"I'm sorry that's not enough. Please enter an amount that's higher than or equal to "
-									+ itemCost);
+							"\nI'm sorry that's not enough. Please enter an amount that's higher than or equal to "
+									+ itemCost + ".");
 					moneyGiven = askGiven(moneyGiven);
 				}
 			} while (moneyGiven < itemCost);
-
-//			MAYBE MAKE BOOLEAN ASKING FOR USER TO QUIT IN IF STMT
-
-//			Can use nested while loop but changing while (moneyGiven > itemCost); to
-//			while (moneyGiven < itemCost); fixed the exit when someone entered a lower number
-//			a second time.
-//			
-//			while (moneyGiven < itemCost) {
-//				System.out.println("I'm sorry that's not enough. Please enter an amount that's higher than or equal to "
-//						+ itemCost);
-//				moneyGiven = askGiven(moneyGiven);
-//			} 
-
-
-			// Gets enough money at this point
-			// need to calculate change
 
 			// User Story #4
 			// If the amount tendered is more than the cost of the item, display the number
@@ -80,7 +62,7 @@ public class MakeChange {
 			int coin5;
 			int coin1;
 			double remainder = 0.0;
-			String moneyReturned;
+
 
 			if (moneyGiven == itemCost) {
 				System.out.println("\nThank you for giving exact change.");
@@ -89,112 +71,140 @@ public class MakeChange {
 
 				if ((moneyGiven - itemCost) / 20 != 0 && ((moneyGiven - itemCost) / 20) >= 1) {
 					bill20 = (int) ((moneyGiven - itemCost) / 20);
-					System.out.println("Num $20: " + bill20);
 
 					remainder = (moneyGiven - itemCost) % 20;
+
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + bill20 + " twenty dollar bill,");
+					}
+					else {
+						moneyReturned += (" " + bill20 + " twenty dollar bill.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 10 != 0 && (remainder / 10) >= 1) {
 					bill10 = (int) (remainder / 10);
-					System.out.println("Num $10: " + bill10);
 
 					remainder = (remainder % 10);
+					
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + bill10 + " ten dollar bill,");
+					}
+					else {
+						moneyReturned += (" " + bill10 + " ten dollar bill.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 5 != 0 && (remainder / 5) >= 1) {
 					bill5 = (int) (remainder / 5);
-					System.out.println("Num $5: " + bill5);
 
 					remainder = (remainder % 5);
+					
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + bill5 + " five dollar bill,");
+					}
+					else {
+						moneyReturned += (" " + bill5 + " five dollar bill.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 1) >= 1) {
 					bill1 = (int) (remainder / 1);
-					System.out.println("Num $1: " + bill1);
 
 					remainder = (remainder) % 1;
+					
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + bill1 + " one dollar bill,");
+					}
+					else {
+						moneyReturned += (" " + bill1 + " one dollar bill.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.25) >= 1) {
 					coin25 = (int) (remainder / 0.25);
-					System.out.println("Num 0.25: " + coin25);
-
+					
 					remainder = (remainder) % 0.25;
+					
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + coin25 + " quarter,");
+					}
+					else {
+						moneyReturned += (" " + coin25 + " quarter.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.10) >= 1) {
 					coin10 = (int) (remainder / 0.10);
-					System.out.println("Num 0.10: " + coin10);
 
 					remainder = (remainder) % 0.10;
+					
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + coin10 + " dime,");
+					}
+					else {
+						moneyReturned += (" " + coin10 + " dime.");
+					}
 				}
 
 				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.05) >= 1) {
 					coin5 = (int) (remainder / 0.05);
-					System.out.println("Num 0.05: " + coin5);
-
+					
 					remainder = (remainder) % 0.05;
+				
+					// Captures punctuation
+					if (remainder != 0) {
+						moneyReturned += (" " + coin5 + " nicle,");
+					}
+					else {
+						moneyReturned += (" " + coin5 + " nickle.");
+					}
 				}
+				
+				// Changed if condition to account for when remainder needs to round up to get the last cent.
+				// i.e. changed (remainder / 0.01) >= 1 to (remainder / 0.01) != 0
+				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.01) != 0) {
 
-				double coin1Round;
-				
-				// (remainder / 0.01) > 1) putting 1 less penny
-//				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.01) > 1) {
-//					coin1 = (int) (remainder / 0.01);
-//
-//					System.out.println("Remainder pre add: " + remainder);
-//					coin1Round = round ((remainder / 0.01), 2);
-//					// TEST IF WILL CAPTURE MISSED PENNY
-//					if (remainder > 0.00001) {
-//						coin1 += 1;
-//					}
-//					System.out.println("Num 0.01: " + coin1);
-//					System.out.println("Coin1Round: " + coin1Round);
-//
-//					remainder = (remainder) % 0.01;
-//				}
-				
-				if ((moneyGiven - itemCost) / 1 != 0 && (remainder / 0.01) > 1) {
-					
-					// COINS PRE ROUNDING MAYBE DELETE LATER
-					coin1 = (int) (remainder / 0.01);
-					System.out.println("Remainder pre round: " + remainder);
-					
-					// ROUNDING TEST SECTION
-					
-					// Cast as int and * 1000 to get last 3 decimals of original remainder
 					int remainderRound;
+					
+					System.out.println("HIT PENNY");
+					System.out.println("Penny remainder: " + remainder);
+
+					// USE FOR THE FINAL ITERATION!?!?
+					coin1 = (int) (remainder / 0.01);
+
+					// Cast as int and * 1000 to get last 3 decimals of original remainder
 					remainderRound = (int) (remainder * 1000);
-					System.out.println("remRoundInt * 1000: " + remainderRound);
 
-					// Divides by 1000 to turn back in to decimal form & rounds to nearest hundredth.
-//					remainder = (double) remainderRound / 1000.0;
-					remainder =  remainderRound / 1000.0;
-					System.out.println("remainder = remRound / 1000: " + remainder);
-					remainder = Math.round(remainder *100.0)/100.0;
-					System.out.println("remainder post Math.round: " + remainder);
-					
-					// 
-					coin1Round = (int) (remainder / 0.01);
-					System.out.println("Num 0.01: " + coin1);
-					System.out.println("Post round 0.01 coins: " + coin1Round);
-					
-					// NOT WORKING remainder = Math.round(remainder, 2);
-					//coin1Round = (int) (remainder / 0.01);
-					
+					// Divides by 1000 to turn back in to decimal form 0.xxx & rounds to nearest
+					// hundredth.
+					remainder = remainderRound / 1000.0;
+					remainder = Math.round(remainder * 100.0) / 100.0;
 
+					coin1 = (int) (remainder / 0.01);
+					
+					// Captures punctuation
+					if(coin1 > 1) {
+						moneyReturned += (" " + coin1 + " pennies.");
+					}
+					else {
+						moneyReturned += (" " + coin1 + " penny.");
+					}
 
-					remainder = (remainder) % 0.01;
 				}
+
 				
-
 			}
+			
+			System.out.println("\n" + moneyReturned);
 
-			System.out.println("\n post round Reminder: " + remainder);
-			System.out.println("\nmoneyGiven " + moneyGiven);
-			System.out.println("itemCost: " + itemCost);
-
-			System.out.print("Would you like to purchase something else? (Y or N) ");
+			System.out.print("\nWould you like to purchase something else? (Y or N) ");
 			again = kb.nextLine();
 
 			switch (again) {
@@ -202,16 +212,21 @@ public class MakeChange {
 			case "n":
 				repeat = false;
 				break;
+			case "Y":
+			case "y":
+				System.out.println();
+				break;
 			default:
 				System.out.println("\nThat wasn't an option and it looks like you have something else to purchase!\n");
 			}
 
 		}
-		while (repeat == true);
+		while (repeat == true)
+			;
 
 		System.out.println("\nThank you for shopping at DVS! Come again soon!");
 		System.out.println("Program exited.");
-		kb.close();		// Close Scanner.
+		kb.close(); // Close Scanner.
 	}
 
 	public static double askCost(double itemCost) {
